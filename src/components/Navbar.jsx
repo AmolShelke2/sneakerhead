0a1e3e355f7d3b0a8e1071c5c5a08412d1e0bbef
Amol shelke <shelkeamol897@gmail.com>
2023-05-22 21:58:34 +0530

    [Feat]: styled navbar for mobile and desktop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 
 const Navbar = () => {
+  const [activeNavbar, setactiveNavbar] = useState(false);
+
   return (
     <div className="w-full bg-black px-12 py-6">
-      <nav>
+      <nav className="flex flex-col md:flex-row justify-between items-center text-center">
         <div className="flex justify-start">
           <h1 className="text-white text-xl">SneakerHeads</h1>
+        </div>
+        <div>
+          <ul className="flex flex-col h-screen md:h-auto justify-center items-center md:flex-row md:justify-between text-white">
+            <li className="mb-6 md:mb-0 md:mr-4">
+              <a href="#">Home</a>
+            </li>
+            <li className="mb-6 md:mb-0 md:mr-4">
+              <a href="#">Collection</a>
+            </li>
+            <li className="mb-6 md:mb-0 md:mr-4">
+              <a href="#">About us</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
         </div>
       </nav>
     </div>
