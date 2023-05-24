5ed28bb5352d4439487ccf2744f06f49a74a743f
Amol shelke <shelkeamol897@gmail.com>
2023-05-24 23:38:12 +0530

    [feat]: Hero component created for small screen device

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="w-full">
       <Navbar />
+      <Hero />
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
+import hero from "../assets/images/hero1.png";
+
 const Hero = () => {
   return (
-    <div>
-      <h1>Join the sneakerHead community</h1>
+    <div className="flex flex-col-reverse justify-center items-center px-6 h-screen md:flex-row md:justify-between md:px-28">
+      <div>
+        <h1>Join the sneakerHead community</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
+          neque sapiente nostrum dolores suscipit ex?
+        </p>
+        <a href="#">see more</a>
+      </div>
+
+      <div className="mb-8 md:mb-0">
+        <img src={hero} alt="hero-image" />
+      </div>
     </div>
   );
 };
