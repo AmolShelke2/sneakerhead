e583dd22c51ee51d85614a4ffe5f33067c45cb87
Amol shelke <shelkeamol897@gmail.com>
2023-05-29 00:17:03 +0530

    [update]: minor styles added

--- src/components/Aboout.jsx
+++ src/components/Aboout.jsx
@@ -8,18 +8,22 @@ const Aboout = () => {
   return (
     <div className="px-4 lg:px-12 mx-auto py-4" id="about-us">
       <h1 className="text-2xl font-bold mb-2">About Us</h1>
-      <div className="flex flex-row-reverse justify-between items-center gap-8">
+      <div className="flex flex-col-reverse sm:flex-row-reverse justify-between items-center gap-8">
         <div>
           <h2 className="text-2xl font-extralight mb-6 shadow-sm">
             What is SneakerHead community?
           </h2>
-          <p>
+          <p className="text-md font-light mb-8 w-[90%]">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis
             commodi unde delectus obcaecati maiores, animi laudantium ratione
             harum odit iure repellendus quasi mollitia molestias eos maxime?
             Tempora eveniet velit omnis.
           </p>
-          <a href="#">know more</a>
+          <a
+            href="#"
+            className="bg-gray-800 px-12 py-4 rounded-md text-white capitalize md:px-12">
+            know more
+          </a>
         </div>
 
         <div className="grid grid-cols-2 gap-5">
