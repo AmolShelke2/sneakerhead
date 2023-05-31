0a51e1465519dcdb702c3e4ff1c776d5ce6edee4
Amol shelke <shelkeamol897@gmail.com>
2023-05-31 22:49:59 +0530

    [Update]: renamed assets and and imported

--- src/components/InstaFeed.jsx
+++ src/components/InstaFeed.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+import Instagram1 from "../assets/images/InstaFeed/Instagram1.jpg";
+import Instagram2 from "../assets/images/InstaFeed/Instagram2.jpg";
+import Instagram3 from "../assets/images/InstaFeed/Instagram3.jpg";
+import Instagram4 from "../assets/images/InstaFeed/Instagram4.mp4";
+import Instagram5 from "../assets/images/InstaFeed/Instagram5.jpg";
+import Instagram6 from "../assets/images/InstaFeed/Instagram6.jpg";
+import Instagram7 from "../assets/images/InstaFeed/Instagram7.jpg";
+import Instagram8 from "../assets/images/InstaFeed/Instagram8.jpg";
+import Instagram9 from "../assets/images/InstaFeed/Instagram9.jpg";
+
 const InstaFeed = () => {
-  return <div>InstaFeed</div>;
+  return (
+    <div>
+      <h2>Our Instagram</h2>
+      <div></div>
+    </div>
+  );
 };
 
 export default InstaFeed;
