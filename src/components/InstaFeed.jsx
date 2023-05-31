1b900950dd6d5a23b8483c11ec5ead659335e16c
Amol shelke <shelkeamol897@gmail.com>
2023-05-31 23:25:21 +0530

    [feat]: completed InstaFeed component

--- src/components/InstaFeed.jsx
+++ src/components/InstaFeed.jsx
@@ -3,18 +3,58 @@ import React from "react";
 import Instagram1 from "../assets/images/InstaFeed/Instagram1.jpg";
 import Instagram2 from "../assets/images/InstaFeed/Instagram2.jpg";
 import Instagram3 from "../assets/images/InstaFeed/Instagram3.jpg";
-import Instagram4 from "../assets/images/InstaFeed/Instagram4.mp4";
+import Instagram4 from "../assets/images/InstaFeed/Instagram4.jpg";
 import Instagram5 from "../assets/images/InstaFeed/Instagram5.jpg";
 import Instagram6 from "../assets/images/InstaFeed/Instagram6.jpg";
 import Instagram7 from "../assets/images/InstaFeed/Instagram7.jpg";
 import Instagram8 from "../assets/images/InstaFeed/Instagram8.jpg";
 import Instagram9 from "../assets/images/InstaFeed/Instagram9.jpg";
 
+const InstaFeedData = [
+  {
+    image: Instagram1,
+  },
+  {
+    image: Instagram2,
+  },
+  {
+    image: Instagram3,
+  },
+  {
+    image: Instagram4,
+  },
+  {
+    image: Instagram5,
+  },
+  {
+    image: Instagram6,
+  },
+  {
+    image: Instagram7,
+  },
+  {
+    image: Instagram8,
+  },
+  {
+    image: Instagram9,
+  },
+];
+
 const InstaFeed = () => {
   return (
-    <div>
-      <h2>Our Instagram</h2>
-      <div></div>
+    <div className="px-4 lg:px-12 mx-auto py-4 mb-5" id="InstaFeed">
+      <h2 className="text-2xl font-bold mb-2">Our Instagram</h2>
+      <div className="grid grid-cols-2 md:grid-cols-3">
+        {InstaFeedData.map((instafeed) => (
+          <div className="m-1 shadow-sm shadow-black">
+            <img
+              src={instafeed.image}
+              alt="instafeed"
+              className="object-cover h-full w-full"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
