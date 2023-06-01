86c8bec9de28d29fdae1ca83de06678d335fcee8
Amol shelke <shelkeamol897@gmail.com>
2023-06-01 23:56:37 +0530

    [feat]: created contact us

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 
+import FormImage from "../assets/images/InstaFeed/Instagram8.jpg";
+
 const Contact = () => {
   return (
-    <div>
-      <h2>Contact us</h2>
-      <div></div>
+    <div
+      className="px-4 lg:px-12 py-4 mb-5 flex flex-col justify-center items-center"
+      id="contact">
+      <h2 className="text-2xl font-bold mb-2">Contact us</h2>
+      <div className="grid grid-cols-2 place-items-start w-[40vw] h-[400px] bg-blue-200">
+        <div className="flex flex-col justify-center h-full p-6">
+          <form className="flex flex-col items-start">
+            <label htmlFor="input">Name</label>
+            <input type="text" placeholder="Enter Your Name" />
+          </form>
+        </div>
+        <div>
+          <img
+            src={FormImage}
+            alt=""
+            className="h-[400px] w-full object-cover"
+          />
+        </div>
+      </div>
     </div>
   );
 };
