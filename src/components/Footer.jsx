10c26b2fe4012b703efb1ad60cc1c2da3128f2fb
Amol <shelkeamol897@gmail.com>
2023-06-12 23:55:40 +0530

    [feat]: Footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex flex-col w-full px-4 py-10 bg-black text-white md:flex-row-reverse md:justify-between items-center">
+      <div>
+        <ul className="flex justify-between">
+          <li className="mr-2">
+            <a href="#">contact</a>
+          </li>
+          <li className="mr-2">
+            <a href="#">more info</a>
+          </li>
+          <li className="mr-2">
+            <a href="#">visit us</a>
+          </li>
+          <li>
+            <a href="#">shop</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p className="mt-2 text-center">
+          All copyrights reserved by <span>Amol Shelke</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
